f1935f44e7e29a3d5647d97009261e9db51e3167
VRS03 <saudagar.viraj@gmail.com>
2024-07-12 18:12:55 -0500

    working getting player stats for hall of fame players with hard-coded hall of fame names.

--- main.py
+++ main.py
@@ -4,19 +4,37 @@ from nba_api.stats.static import players
 from nba_api.stats.endpoints import playercareerstats
 import pandas as pd
 
-
-def get_hall_of_fame_players():
-    allPlayers = players.get_players()
-    hallOfFamePlayers = []
-
-    for player in allPlayers:
-        if player['is_hall_of_famer']:
-            hallOfFamePlayers.append(player)
-
-    return hallOfFamePlayers
+hall_of_fame_names = [
+    "Michael Jordan",
+    "Magic Johnson",
+    "Larry Bird",
+    "Kareem Abdul-Jabbar",
+    "Wilt Chamberlain",
+]
 
 
-# Main
-print("Test")
-hallOfFamePlayers = get_hall_of_fame_players()
-print(hallOfFamePlayers)
+def get_player_stats(player_id):
+    career = playercareerstats.PlayerCareerStats(player_id=player_id)
+    return career.get_data_frames()[0]
+
+
+def get_player_id(name):
+    player_dict = players.find_players_by_full_name(name)
+    if player_dict:
+        return player_dict[0]['id']
+    return None
+
+
+hof_player_ids = []
+for name in hall_of_fame_names:
+    player_id = get_player_id(name)
+    if player_id:
+        hof_player_ids.append((name, player_id))
+
+hofStats = []
+for name, player_id in hof_player_ids:
+    stats = get_player_stats(player_id)
+    stats['player_name'] = name
+    hofStats.append(stats)
+
+print(hofStats)
