b9e1fec578a4bc61d0dc9deaa26b33cb1e9f156b
VRS03 <saudagar.viraj@gmail.com>
2024-07-12 18:03:17 -0500

    testing file

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
 # NBA Hall of Fame Predictor
 
+from nba_api.stats.static import players
+from nba_api.stats.endpoints import playercareerstats
+import pandas as pd
+
+
+def get_hall_of_fame_players():
+    allPlayers = players.get_players()
+    hallOfFamePlayers = []
+
+    for player in allPlayers:
+        if player['is_hall_of_famer']:
+            hallOfFamePlayers.append(player)
+
+    return hallOfFamePlayers
+
+
+# Main
+print("Test")
+hallOfFamePlayers = get_hall_of_fame_players()
+print(hallOfFamePlayers)
